4828b990225bdf2f882894bfdce209fbc6374b60
apurba <apurba123@gmail.com>
2022-03-10 15:13:30 +0530

    third commit

--- src/com/sample.java
+++ src/com/sample.java
@@ -5,6 +5,8 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Maven Example");
+			System.out.println("This is Java Project");
+
 	}
 
 }
