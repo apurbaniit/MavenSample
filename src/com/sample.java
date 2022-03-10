a981dfe93aaac7ae9c6670a33deb0057d921531f
apurba <apurba123@gmail.com>
2022-03-10 14:27:05 +0530

    second commit

--- src/com/sample.java
+++ src/com/sample.java
@@ -4,7 +4,7 @@ public class sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Maven Example");
+		System.out.println("This is Maven Example");
 	}
 
 }
