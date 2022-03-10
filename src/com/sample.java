ea1c4553ab6e57a4388984b20c4d1fa2e27e9760
apurba <apurba123@gmail.com>
2022-03-10 15:18:37 +0530

    forth commit

--- src/com/sample.java
+++ src/com/sample.java
@@ -5,7 +5,7 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is Maven Example");
-			System.out.println("This is Java Project");
+			System.out.println("This is jenkins Project");
 
 	}
 
